8915e163a54e0236529758e60c61f0b93f268b8f
ajay <ajay.t@equipohealth.com>
2023-10-13 17:01:46 +0530

    localhost url added

--- index.js
+++ index.js
@@ -11,20 +11,26 @@ connectDb()
 
 // CORS configuration
 const corsOptions = {
-    origin: '*', // Change this to your specific origin or origins.
+    origin: 'http://localhost:5173', // Change this to your specific origin or origins.
     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'], // Add the headers you need.
 };
 
 app.use(cors(corsOptions))
 
+// Handle preflight requests (OPTIONS requests)
+app.options('*', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.status(204).send();
+});
+
 app.use(bodyParser.json())
 app.get('/', (req, res) => { res.send('Welcome to chat api') })
 app.use('/chat', chatRouter)
 app.use('/user', userRouter)
 
-// Handle preflight requests (OPTIONS requests)
-app.options('*', cors(corsOptions));
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`server listen on port:${port}`))

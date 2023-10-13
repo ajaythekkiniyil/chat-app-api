0eea3d5efe619738ce9096531008a1194f8303c5
ajay <ajay.t@equipohealth.com>
2023-10-13 15:22:59 +0530

    updated cors config

--- index.js
+++ index.js
@@ -3,21 +3,28 @@ const app = express()
 const chatRouter = require('./routes/chat')
 const userRouter = require('./routes/user')
 const connectDb = require('./controller/db')
-const cors = require('cors')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 
 connectDb()
-app.use(cors(
-    {
-        origin: '*',
-        methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-    }
-))
+
+// CORS configuration
+const corsOptions = {
+    origin: '*', // Change this to your specific origin or origins.
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'], // Add the headers you need.
+};
+
+app.use(cors(corsOptions))
+
 app.use(bodyParser.json())
 app.get('/', (req, res) => { res.send('Welcome to chat api') })
 app.use('/chat', chatRouter)
 app.use('/user', userRouter)
+
+// Handle preflight requests (OPTIONS requests)
+app.options('*', cors(corsOptions));
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`server listen on port:${port}`))
